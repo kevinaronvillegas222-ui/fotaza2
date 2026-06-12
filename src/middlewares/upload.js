d98e54412f63582e3eea9512898c4818ac4014a1
Villegas Kevin Aron <kevinvillegas252@gmail.com>
2026-06-12 04:23:31 -0300

    corregir exportacion de uploadImages

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,13 +1,13 @@
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('cloudinary').v2;
-
+ 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
+ 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
@@ -16,7 +16,7 @@ const storage = new CloudinaryStorage({
     transformation: [{ width: 1200, crop: 'limit' }],
   },
 });
-
-const upload = multer({ storage });
-
-module.exports = { upload, cloudinary };
+ 
+const uploadImages = multer({ storage });
+ 
+module.exports = { uploadImages, cloudinary };
